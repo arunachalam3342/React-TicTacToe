612fe5b65381d9dafb0dceb5f1360a565afd650e
arunachalam3342 <105549877+arunachalam3342@users.noreply.github.com>
2023-06-16 15:30:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Task03 from "./components/Task03"
+import Task04 from "./components/Task04"
 function App() {
-  const colors = ['Red','Yellow','Orange','Green', 'Blue', 'Violet', 'Purple','Black'];
+  
   return (
     <div className="App">
-     <Task03 colors={colors}/>
+     <Task04 />
     </div>
   );
 }
